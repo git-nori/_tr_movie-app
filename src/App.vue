9864df94a0b3eacdc47ccf2855935085aa761ffe
HironoriSato <maza.hs5360@gmail.com>
2019-09-26 21:59:40 +0900

    [add]app-varに検索フォームを新規作成

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
   <v-app>
-    <v-app-bar app></v-app-bar>
+    <v-app-bar app>
+      <v-container fruid>
+        <v-row sm="6" md="3" class="justify-center mt-3">
+          <v-form v-model="valid" class="mx-3">
+            <v-text-field
+              label="movieName"
+              required
+              :rules="searchWordRules"
+              prepend-icon="mdi-movie"
+              v-model="searchWord"
+            >test</v-text-field>
+          </v-form>
+          <v-btn class="mx-3" :disabled="!dataAvailable" @click="searchMovie">
+            <v-icon left>mdi-magnify</v-icon>
+            <span>SEARCH</span>
+          </v-btn>
+        </v-row>
+      </v-container>
+    </v-app-bar>
     <v-content>
       <router-view></router-view>
     </v-content>
@@ -10,8 +28,23 @@
 <script>
 export default {
   name: "App",
-  data: () => ({
-    //
-  })
+  data() {
+    return {
+      searchWord: "",
+      valid: false,
+      searchWordRules: [v => !!v || "inputWord is required"]
+    };
+  },
+  methods: {
+    searchMovie() {
+      this.$router.push("/search/" + this.searchWord);
+      this.searchWord = "";
+    }
+  },
+  computed: {
+    dataAvailable() {
+      return this.searchWord !== null && this.searchWord !== "";
+    }
+  }
 };
 </script>
