579b23dc748a48faa9a19c7a4fd410386a1caeea
HironoriSato <maza.hs5360@gmail.com>
2019-09-26 23:28:09 +0900

    [mod]検索結果が存在するかどうかで表示を切り替えるよう修正

--- src/components/SearchMovie.vue
+++ src/components/SearchMovie.vue
@@ -3,6 +3,11 @@
     <div class="text-center" v-if="loading">
       <v-progress-circular indeterminate size="150" width="8" color="green"></v-progress-circular>
     </div>
+    <div v-else-if="isNoData">
+      <div class="text-center">
+        <h2>No Movie in API with</h2>
+      </div>
+    </div>
     <div v-else>
       <v-row>
         <v-col sm="6" md="4" lg="3" v-for="(item, index) in movieResponse" :key="index">
@@ -36,6 +41,7 @@ export default {
   data() {
     return {
       loading: true,
+      isNoData: false,
       movieResponse: []
     };
   },
@@ -55,7 +61,13 @@ export default {
         axios
           .get(targetUrl)
           .then(response => {
-            this.movieResponse = response.data.Search;
+            // APIのデータが返ってきているか判定
+            if (response.data.Response === "True") {
+              this.movieResponse = response.data.Search;
+              this.isNoData = false;
+            } else {
+              this.isNoData = true;
+            }
             this.loading = false;
           })
           .catch(error => {
